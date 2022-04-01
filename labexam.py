3e0787bdda2f04e139faaefa814dcce1ca1d4368
Gopika1 <74651932+Gopika1@users.noreply.github.com>
2022-04-01 11:25:48 +0530

    Add files via upload

---
+++ labexam.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+a=np.array([[1,2,3,4],[5,6,7,8],[9,1,2,3],[4,5,7,8]])
+print("Original Array \n",a)
+print("Display all elements excluding the first row")
+print(a[1:])
+print("Display the 3rd and 4th element of 1st row")
+print(a[0,2],a[0,3])
+
